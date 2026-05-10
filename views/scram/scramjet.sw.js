db0d5026aaf7b2dc1b1ce414c787e0fa31eff862
7hbl <hellonerd1@yahoo.com>
2026-05-10 00:11:27 -0400

    m

--- views/scram/scramjet.sw.js
+++ views/scram/scramjet.sw.js
@@ -1,4 +1,4 @@
-importScripts('{{route}}{{/scram/scramjet.all.js}}');
+importScripts('/worker/working.all.js');
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
@@ -10,41 +10,28 @@ async function handleRequest(event) {
   return fetch(event.request);
 }
 
+self.addEventListener('install', () => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event));
 });
 
-let playgroundData;
 self.addEventListener('message', (event) => {
-  if (event.data.type === 'playgroundData') {
-    playgroundData = event.data;
-  } else if (event.data.type === 'requestAC') {
-    // Set up a message channel from common.js to process autocomplete search results.
+  if (event.data.type === 'requestAC') {
     const requestPort = event.ports[0];
     requestPort.addEventListener('message', async (event) => {
       const response = await scramjet.fetch(event.data);
-
-      // This contains some duplicate code from common.js, since Response objects
-      // cannot be passed through service workers and must be preprocessed.
       const responseType = response.headers.get('content-type');
       let responseJSON = {};
       if (responseType && responseType.indexOf('application/json') !== -1)
         responseJSON = await response.json();
-      else
-        try {
-          responseJSON = await response.text();
-          try {
-            responseJSON = JSON.parse(responseJSON);
-          } catch (e) {
-            responseJSON = JSON.parse(
-              responseJSON.replace(/^[^[{]*|[^\]}]*$/g, '')
-            );
-          }
-        } catch (e) {
-          // responseJSON will be an empty object if everything was invalid.
-        }
-
-      // Return the processed data.
+      else try { responseJSON = JSON.parse(await response.text()); } catch (e) {}
       requestPort.postMessage({
         responseJSON: responseJSON,
         searchType: event.data.type,
